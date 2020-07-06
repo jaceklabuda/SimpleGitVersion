0b652ad440ea3b8f235e5a0e3aeb3b6e6b511a58
Jacek Labuda <labuda.jacek@gmail.com>
2020-07-06 06:52:15 +0200

    Auto increment a level of version
    * #4 Auto increment a level of version
    
    * #4 #8 Describe tests for existing features
    
    * #9 Show more usages, add sections develop & contribute
    
    * #9 Show more usages, add sections develop & contribute

--- SimpleGitVersion.ps1
+++ SimpleGitVersion.ps1
@@ -3,25 +3,50 @@ function Calculate-Version
 	[CmdletBinding()] 
     Param 
     ( 
-        [Parameter(Mandatory=$false)] 
-        [ValidateNotNullOrEmpty()]
+        [Parameter(Mandatory=$false)]
+        [string]$prefix='',
+        [Parameter(Mandatory=$false)]
         [string]$preRelease,        
         [Parameter(Mandatory=$false)] 
-        [ValidateNotNullOrEmpty()]
-		[string]$preReleaseNumber
+		[string]$preReleaseNumber,        
+        [Parameter(Mandatory=$false)] 
+		[string]$autoIncrementLevel
     )
 	Process
 	{
-		$prefix = 'v'
         $version = '0.1.0'
-
-        $command = 'git describe --match "v*" --abbrev=0 --tags --exclude "*-*" $(git rev-list --tags --max-count=1)'
+        $command = 'git describe --match "$prefix*" --abbrev=0 --tags --exclude "*-*" $(git rev-list --tags --max-count=1)'
 
         $result = Invoke-Expression $command
-
-        if ($result.length -ne 0)
+        
+        if ($result.length -ne 0 -and $prefix.length -ne 0)
         {
             $version = $result.replace($prefix, '')
+        }        
+
+        if ($result.length -ne 0 -and $autoIncrementLevel.length -ne 0)
+        {
+            $major,$minor,$patch = $version.Split('.')
+
+            if ($autoIncrementLevel -eq 'major')
+            {
+                $major = 1 + $major
+                $minor = 0
+                $patch = 0
+            }
+
+            if ($autoIncrementLevel -eq 'minor')
+            {
+                $minor = 1 + $minor
+                $patch = 0
+            }
+            
+            if ($autoIncrementLevel -eq 'patch')
+            {
+                $patch = 1 + $patch
+            }
+
+            $version = "$major.$minor.$patch"
         }
 
         if ($preRelease.length -ne 0)
